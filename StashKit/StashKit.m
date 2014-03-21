3175fdc0b684c3e6117e48f52638d1b2fc381e8d
Romain Pouclet <romain.pouclet@techsolcom.ca>
2014-03-21 14:51:52 -0400

    Initial Commit

---
+++ StashKit/StashKit.m
@@ -0,0 +1,13 @@
+//
+//  StashKit.m
+//  StashKit
+//
+//  Created by Romain Pouclet on 2014-03-21.
+//  Copyright (c) 2014 Perfectly-Cooked. All rights reserved.
+//
+
+#import "StashKit.h"
+
+@implementation StashKit
+
+@end
