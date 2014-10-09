f7ce92ad319ddf0b2996468fc3903728265556ca
Romain Pouclet <palleas@gmail.com>
2014-10-09 18:49:58 -0400

    Markdown. Meh.

--- README.markdown
+++ README.markdown
@@ -8,10 +8,10 @@ The easiest way is probably to use cocoapods.
 
 ## Getting started
 
-	````objective-c
+	```objectivec
 	NSURL *endpoint = [NSURL URLWithString: @"http://stash.oa"];
 	STKClient *client = [[STKClient alloc] initWithUsername: @"hal.jordan" password: @"b3w4r3" baseUrl: endpoint];
-	````
+	```
 
 ## Supported methods
 
--- StashKitTests/STKClientSpec.m
+++ StashKitTests/STKClientSpec.m
@@ -6,35 +6,23 @@
 //  Copyright (c) 2014 Perfectly-Cooked. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import <XCTest/XCTest.h>
+#import <Specta/Specta.h>
+#import <Expecta/Expecta.h>
 
-@interface STKClientSpec : XCTestCase
+SpecBegin(STKClient)
 
-@end
+describe(@"Authenticaged", ^{
+    it(@"should fetch a list of projects", ^{
 
-@implementation STKClientSpec
+    });
+    
+    it(@"should create a project", ^{
+    
+    });
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
+    it(@"should create a repository for a given project", ^{
+        
+    });
+});
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
-@end
+SpecEnd
